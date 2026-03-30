91fbce4aeeee03359d627875dd8d785ba63acc99
Rishank <rishank.sharma.1426@gmail.com>
2026-03-31 03:49:26 +0530

    model

---
+++ src/main/java/com/example/ToDoList/model/ToDo.java
@@ -0,0 +1,40 @@
+package com.example.ToDoList.model;
+
+public class ToDo {
+
+    private int id;
+    private String title;
+    private Boolean completed;
+
+    public ToDo(){};
+
+    public ToDo(int id, String title, Boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+}
